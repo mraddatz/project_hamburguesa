95cc69a5bfbb414b17ab47972225f3f015e394cf
Martin Raddatz <martinraddatz@MacBook-Pro-de-Martin.local>
2020-05-07 10:38:33 -0400

    env variable testing

--- apiapp/views.py
+++ apiapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to Hamburger API Homepage. Here will be the documentation")
+    print("Entered index")
+    my_variable = os.environ.get('ENV_VARIABLE')
+    string = "Welcome to Hamburger API Homepage. Here will be the documentation + "
+    string_env = string + my_variable
+    return HttpResponse(string_env)
+
+
--- project_hamburguesa/settings.py
+++ project_hamburguesa/settings.py
@@ -36,6 +36,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
